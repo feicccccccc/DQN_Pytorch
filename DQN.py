17acc21fb265681da663205316cf62a2f8784146
fei <feicheung2016@gmail.com>
2020-03-29 15:36:11 +0800

    Setting up projets, inital comit

---
+++ DQN.py
@@ -0,0 +1,14 @@
+"""
+Exercise for Implementing DQN
+following:
+Pytorch Documentation: https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+DeepMind Youtube lecture: https://www.youtube.com/watch?v=2pWv7GOvuf0&list=PLqYmG7hTraZDM-OYHWgPebj2MfCFzFObQ
+
+"""
+
+import torch
+import gym
+import numpy as np
+from matplotlib.pyplot import plt
+
+
